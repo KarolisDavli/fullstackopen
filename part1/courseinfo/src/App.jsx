39f611f963021194bf3c772a91581164a6c6adbe
Karolis <davlijevas.karolis@gmail.com>
2025-10-30 12:50:31 +0200

    fix par1 solutions

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,11 +1,58 @@
-import {useState} from "react";
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      {props.parts.map((part) => (
+        <Part name={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Total = (props) => {
+  const getTotal = () =>
+    props.parts.reduce((sum, part) => sum + part.exercises, 0);
+  const total = getTotal();
+  return <p>Number of exercises {total}</p>;
+};
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
-  setTimeout(() => setCounter(counter + 1), 1000);
-
-  return <div>{counter}</div>;
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
 };
 
 export default App;
--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -39,24 +39,11 @@ const Statistics = (props) => {
 };
 
 function App() {
-  const anecdotes = [
-    "If it hurts, do it more often.",
-    "Adding manpower to a late software project makes it later!",
-    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-    "Premature optimization is the root of all evil.",
-    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
-    "The only way to go fast, is to go well.",
-  ];
-
-  const [selected, setSelected] = useState(0);
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
   const [all, setAll] = useState(0);
   const [averageScore, setAverageScore] = useState(0);
-  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
 
   const feedback = (rating) => {
     const handler = () => {
@@ -80,25 +67,9 @@ function App() {
     return handler;
   };
 
-  const getRandom = () => Math.floor(Math.random() * anecdotes.length);
-
-  const getRandomAnecdote = () => {
-    setSelected(getRandom);
-  };
-
-  const vote = () => {
-    const copy = [...votes];
-    copy[selected] += 1;
-    setVotes(copy);
-  };
-
   const averageScoreCount = all !== 0 ? averageScore / all : 0;
   const positivePercentage = (partialValue, totalValue) =>
     totalValue ? `${(100 * partialValue) / totalValue}%` : "0%";
-
-  const topScore = Math.max(...votes);
-  const topAnecdoteIndex = votes.indexOf(topScore);
-  console.log("topAnecdoteIndex", topAnecdoteIndex);
 
   return (
     <>
@@ -114,13 +85,6 @@ function App() {
         averageScoreCount={averageScoreCount}
         positivePercentage={positivePercentage(good, all)}
       />
-      <h2>Anecdote of the day</h2>
-      <p>{anecdotes[selected]}</p>
-      <p>has {votes[selected]} votes</p>
-      <button onClick={vote}>Vote</button>
-      <button onClick={getRandomAnecdote}>Next anecdote</button>
-      <h2>Anecdote with most votes</h2>
-      <p>{anecdotes[topAnecdoteIndex]}</p>
     </>
   );
 }
